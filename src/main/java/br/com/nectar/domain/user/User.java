97eeacf37c1bd7b9ad628953b58194bf5a355719
Edson Alencar <edson.cesar.alencar@gmail.com>
2024-12-03 16:55:20 -0300

    first. commit

---
+++ src/main/java/br/com/nectar/domain/user/User.java
@@ -0,0 +1,48 @@
+package br.com.nectar.domain.user;
+
+import br.com.nectar.domain.auth.Auth;
+import br.com.nectar.domain.privilege.Privilege;
+import br.com.nectar.domain.profile.Profile;
+import br.com.nectar.domain.role.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    private UUID id = UUID.randomUUID();
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "auth_id", referencedColumnName = "id")
+    private Auth auth;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", referencedColumnName = "id")
+    private Profile profile;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    private Role role;
+
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "privileges_on_users",
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "privilege_id", referencedColumnName = "id")
+    )
+    private List<Privilege> privileges;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
---
+++ src/test/java/br/com/nectar/NectarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nectar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NectarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
