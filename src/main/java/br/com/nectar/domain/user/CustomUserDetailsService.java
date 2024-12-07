c49b481e9f87de479d1af6d35657b7649b616611
João Neto <jnetogoncalo@gmail.com>
2024-12-06 21:52:13 -0300

    fix: service annotation in UserDetailsService

--- src/main/java/br/com/nectar/domain/user/CustomUserDetailsService.java
+++ src/main/java/br/com/nectar/domain/user/CustomUserDetailsService.java
@@ -2,13 +2,17 @@ package br.com.nectar.domain.user;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
