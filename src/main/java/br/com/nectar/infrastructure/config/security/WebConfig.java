c597faf7ecc1a3a5f219594da56cf5be29668503
j0ngs <jnetogoncalo@gmail.com>
2024-12-08 17:26:23 -0300

    att cors

--- src/main/java/br/com/nectar/infrastructure/config/security/WebConfig.java
+++ src/main/java/br/com/nectar/infrastructure/config/security/WebConfig.java
@@ -1,13 +1,17 @@
 package br.com.nectar.infrastructure.config.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.nectar.domain.token.JwtAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig {
@@ -28,17 +32,30 @@ public class WebConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        .csrf(csrf -> csrf.disable())
-        .cors(cors -> cors.disable())  
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(ROLES + "**").hasRole("ORG")
-                        .requestMatchers(USERS + "login").permitAll()
-                        .requestMatchers(USERS + "register").hasRole("ORG")
-                        .anyRequest().authenticated())
-                .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(ROLES + "**").hasRole("ORG")
+                .requestMatchers(USERS + "login").permitAll()
+                .requestMatchers(USERS + "register").hasRole("ORG")
+                .anyRequest().authenticated())
+            .sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // Permite todas as origens;
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setExposedHeaders(List.of("Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Aplica as regras a todos os endpoints
+        return source;
     }
 }
