6922067b101748827a01d2f2d420d838b86a23f2
João Neto <jnetogoncalo@gmail.com>
2024-12-11 02:03:27 -0300

    att web config

--- src/main/java/br/com/nectar/infrastructure/config/security/WebConfig.java
+++ src/main/java/br/com/nectar/infrastructure/config/security/WebConfig.java
@@ -48,9 +48,8 @@ public class WebConfig {
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/error").anonymous()
                 .requestMatchers(USERS + "login").permitAll()
-                .requestMatchers(USERS + "register").permitAll()
-                .requestMatchers(USERS + "{userId}/update-password").permitAll()
-                .requestMatchers(USERS + "{userId}/update-username").permitAll()
+                .requestMatchers(USERS + "{userId}/update-password").hasRole(MANAGER)
+                .requestMatchers(USERS + "{userId}/update-username").hasRole(MANAGER)
                 .requestMatchers(USERS + "**").hasRole(ORG)
                 .requestMatchers(BEEKEPEERS + "**").hasRole(MANAGER)
                 .requestMatchers(ROLES + "**").hasRole(ORG)
