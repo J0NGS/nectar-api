23bfcd8028daaa78f6adab1e6208258f54f89451
João Neto <jnetogoncalo@gmail.com>
2025-01-27 16:05:27 -0300

    feat add querys de busca para job

--- src/main/java/br/com/nectar/domain/job/JobRepository.java
+++ src/main/java/br/com/nectar/domain/job/JobRepository.java
@@ -9,10 +9,37 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, UUID> {
+    @Query("""
+        SELECT j FROM Job j
+        WHERE j.beekeeper.id = :beekeeperId
+    """)
+    Page<Job> findByBeekeeperName(@Param("beekeeperName") String beekeeperId, Pageable pageable);
+    
+
+    @Query("""
+        SELECT j FROM Job j
+        WHERE j.owner.id = :ownerId
+    """)
+    Page<Job> findByOwnerId(@Param("ownerId") UUID ownerId, Pageable pageable);
+    
+
+    @Query("""
+        SELECT j FROM Job j
+        WHERE j.org.id = :orgId
+    """)
+    Page<Job> findByOrgId(@Param("orgId") UUID orgId, Pageable pageable);
+    
+
+    @Query("""
+        SELECT j FROM Job j
+        WHERE j.id = :jobId
+    """)
+    Optional<Job> findById(@Param("jobId") UUID jobId);
 
     @Query("SELECT j FROM Job j where j.org.id = :userOrgId AND j.status IN :statuslist")
     Page<Job> getPageByStatus(
